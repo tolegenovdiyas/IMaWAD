3775ba7ece6eaccd40e98f354c01fdd0503c98d1
tolegenovdiyas <145113148+tolegenovdiyas@users.noreply.github.com>
2024-10-21 18:47:37 +0600

    Add files via upload

---
+++ 5/sounds.js
@@ -0,0 +1,30 @@
+function soundred(){
+ const audio = new Audio(`sounds/A.wav`);
+ audio.play()
+
+}
+
+function soundorange(){
+    const audio = new Audio(`sounds/B.wav`);
+    audio.play()
+}
+
+function soundyellow(){
+    const audio = new Audio(`sounds/C.wav`);
+    audio.play()
+}
+
+function soundgreen(){
+    const audio = new Audio(`sounds/D.wav`);
+    audio.play()
+}
+
+function soundblue(){
+    const audio = new Audio(`sounds/E.wav`);
+    audio.play()
+}
+
+function soundpurple(){
+    const audio = new Audio(`sounds/F.wav`);
+    audio.play()
+}
